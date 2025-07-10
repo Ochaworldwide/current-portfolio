35d98c3c48c7e6c9d93503a5aae19d2d5de29ff8
Ochaworldwide <lazarusgabriel000@gmail.com>
2025-07-10 14:14:19 +0100

    first commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -36,7 +36,7 @@ class About extends Component {
               <h2>About Me</h2>
 
               <p>
-                Hello! I'm <span style={{color:"#11ABB0"}}>Ibangha Ike</span>, a dedicated and creative Full Stack Developer on a mission to bring ideas to life through elegant and efficient code. With a foundation in <span style={{color:"#11ABB0"}}>React</span>, <span style={{color:"#11ABB0"}}>TailwindCSS</span>, <span style={{color:"#11ABB0"}}>PHP/Laravel</span>, <span style={{color:"#11ABB0"}}>NodeJS/Express</span>, and a flair for design, I thrive in turning concepts into seamless digital experiences. Let's build something extraordinary together!
+                Hello! I'm <span style={{color:"#11ABB0"}}>Lazarus Gabriel</span>, a dedicated and creative Full Stack Developer on a mission to bring ideas to life through elegant and efficient code. With a foundation in <span style={{color:"#11ABB0"}}>React</span>, <span style={{color:"#11ABB0"}}>TailwindCSS</span>, <span style={{color:"#11ABB0"}}>Tanstack</span>, and a flair for design, I thrive in turning concepts into seamless digital experiences. Let's build something extraordinary together!
               </p>
               <div className="row">
                 <div className="columns contact-details">
